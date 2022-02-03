0a2dd33f6bef9eb1428c55e9963fc3b1cde2d48d
ethanimperial <2001200A@student.tp.edu.sg>
2022-02-03 15:39:40 +0800

    created user.java model and user.dao to retrieve user details

--- EcommerceWebsite/src/main/java/ec/ecommerce/model/User.java
+++ EcommerceWebsite/src/main/java/ec/ecommerce/model/User.java
@@ -1,52 +1,63 @@
 package ec.ecommerce.model;
 
 public class User {
+	private int id;
+	private String username;
+	private String email;
+	private String password;
+	private String phonenumber;
 	
-	public User(String name, String password, String email, String phonenumber, int id) {
-		this.name = name;
-		this.password = password;
-		this.email = email;
-		this.phonenumber = phonenumber;
-		this.id = id;
-	}
 	public User() {
-		// TODO Auto-generated constructor stub
+		
 	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPhonenumber() {
-		return phonenumber;
-	}
-	public void setPhonenumber(String phonenumber) {
-		this.phonenumber = phonenumber;
-	}
+	
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
-	protected String name;
-	 protected String password;
-	 protected String email;
-	 protected String phonenumber;
-	 protected int id;
 
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+
+	public User(int id, String username, String email, String password, String phonenumber) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.email = email;
+		this.password = password;
+		this.phonenumber = phonenumber;
+	}
 
 }
